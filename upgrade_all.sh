35853abec3de020f226e4029dd6a2d29fdde19dd
Scott LaCosse <scott.lacosse@syw.com>
2016-09-08 15:08:30 -0500

    updated and simplified to the basics

--- upgrade_all.sh
+++ upgrade_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-#	upgrade_all.sh:	apt-get, gcloud and conda updates in one script
+#	upgrade_all.sh:	apt-get and gcloud updates
 #	author:	scott lacosse
 #
 echo "*"
@@ -23,23 +23,3 @@ echo "***"
 echo "**"
 echo "*"
 gcloud components update
-echo "*"
-echo "**"
-echo "***"
-echo "****"
-echo "***** anaconda update "
-echo "****"
-echo "***"
-echo "**"
-echo "*"
-conda update -y --prefix /home/slacoss/anaconda2 anaconda
-echo "*"
-echo "**"
-echo "***"
-echo "****"
-echo "***** google cloud python libraries update "
-echo "****"
-echo "***"
-echo "**"
-echo "*"
-pip install --upgrade gcloud google-api-python-client
